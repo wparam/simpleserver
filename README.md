c7be7d3e78c5b28268767e8de5ce01dcf9747ee1
wparam@hotmail.com <wparam@hotmail.com>
2016-08-02 22:30:11 +0800

    add some files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # simpleserver
+
+This is a simple functional server, used to add proxy, loadbalance, and using Express in a pure node env.
---
+++ gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt){
+    grunt.initConfig({
+        jsHint: {
+            files: ['server.js', 'app/**/*.js']
+        }
+        watch:{
+            files: ['<%=jsHint.files %>'],
+            tasks: ['jsHint']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['jsHint']);
+};
