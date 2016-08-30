56afe35b5294c381d81e08c0a0f7556271e39b02
wparam@hotmail.com <wparam@hotmail.com>
2016-08-30 22:34:36 +0800

    up

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
-var http = require('http');
+var http = require('http'),
+    mongoClient = require('mongodb').MongoClient,
+    assert = require('assert');
+
+var url = 'mongodb://localhost:27017'
+
+mongoClient.connect(url, function(err, db){
+    assert.equal(null, err);
+    console.log('Connect to server');
+    db.close();
+});
+
 module.exports = function(){
     http.createServer({});
 }
