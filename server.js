befd791b877eb3bf1174b63199fc7744086034cf
wparam@hotmail.com <wparam@hotmail.com>
2016-09-06 22:32:33 +0800

    up

--- server.js
+++ server.js
@@ -7,10 +7,34 @@ var url = 'mongodb://localhost:27017/test'
 mongoClient.connect(url, function(err, db){
     assert.equal(null, err);
     console.log('Connect to server');
-    console.log(db.getCollection('user').find());
-    db.close();
+
+    insertFun(db, function(){
+        db.close();
+    });
+    
 });
 
 module.exports = function(){
     http.createServer({});
 }
+
+
+function insertFun(db, callback){
+    var col = db.collection('user');
+    col.insertMany([
+        {a: 1}, {b:2}
+    ], function(err, result){
+        assert.equal(null, err);
+        assert.equal(2, result.result.n);
+        console.log('inserted 2 documents');
+        callback(result);
+    });
+};
+
+function queryFun(){
+
+};
+
+function deleteFun(){
+
+};
