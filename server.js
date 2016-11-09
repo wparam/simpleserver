e0644acae54f9f0c2b53b227ce9acd68b4b975bc
wparam@hotmail.com <wparam@hotmail.com>
2016-11-09 16:49:28 +0800

    add

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const http = require('http');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 4000;
 
 function Run(){
     var server = http.createServer((req, res)=>{
         //res.statusCode = 200;
         //res.setHeader('Content-Type', 'text/plain');
+        
         res.end('hello, world\n');
     });
 
@@ -19,16 +20,14 @@ function Run(){
     });
 
     var request = http.request({
-        port: 3000,
-        hostname: '127.0.0.1',
+        port: port,
+        hostname: hostname,
         method: 'GET'
     });
     request.on('response', (res)=>{
         console.log('hit response');
     });
-    request.on('socket', ()=>{
-        console.log('socket event');
-    });
+    //request.write('A', 'utf8')
     request.end();
     // http.request().on('response', function(res){
     //     res.on('data', function(data){
