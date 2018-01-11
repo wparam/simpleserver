0f102470303833c5b2cb84dc80484182ad6e8b00
wparam <wparam@hotmail.com>
2018-01-11 17:10:57 -0500

    up

--- app.js
+++ app.js
@@ -2,27 +2,47 @@ var express = require('express')
 var path = require('path')
 var app = express()
 var router = require('./app/routers/core.router')(app)
+const cluster = require('cluster');
+const loadbalancer = require('./modules/loadbalancer');
 
 const mockService = [
-    {host: '115:60016', orgId: 1, clientId: 1, endpoint:'/a'},
-    {host: '115:60016', orgId: 1, clientId: 2, endpoint:'/a'},
-    {host: '115:60016', orgId: 1, clientId: 3, endpoint:'/a'},
-    {host: '115:50015', orgId: 1, clientId: 4, endpoint:'/a'},
-    {host: '115:50015', orgId: 1, clientId: 5, endpoint:'/a'}
+    {host: 'http://172.20.30.115:60016', orgId: 1, clientId: 1, endpoint:'/a', type:'sherpa1'},
+    {host: 'http://172.20.30.115:60016', orgId: 1, clientId: 2, endpoint:'/a', type:'sherpa2'},
+    {host: 'http://172.20.30.115:60016', orgId: 1, clientId: 3, endpoint:'/a', type:'sherpa3'},
+    {host: 'http://172.20.30.115:50015', orgId: 1, clientId: 4, endpoint:'/a', type:'sherpa1'},
+    {host: 'http://172.20.30.115:50015', orgId: 1, clientId: 5, endpoint:'/a', type:'sherpa2'}
 ];
-
-app.set('views', './app/views');
-app.set('view engine', 'pug');
-
-app.use('/static', express.static(path.join(__dirname, 'public')));
-
-app.listen(3001, function(){
-    console.log('server is running');
-});
 
 
 for (var key in mockService) {
     var server = mockService[key];
-    loadbalancer.addServer(server.orgId, server.host, server.endpoint);
+    loadbalancer.addServer(server.orgId + server.type, server.host, server.endpoint);
 }
 loadbalancer.start();
+
+if(cluster.isMaster){
+    var workers = [];
+    for (var id in cluster.workers) {
+        workers.push(cluster.fork());
+    }
+    var cleanExpired = function () {
+        if(workers.length===0){
+            logger.error('Worker List Is Empty!');
+            return;
+        }
+        var workerId = Math.floor((Math.random() * workers.length));
+        console.log('Worker: ' + workers[workerId].id + ' should be cleaning up expired sessions!');
+        workers[workerId].send('cleanExpired');
+    };
+
+    setInterval(cleanExpired, 100000);
+
+    cluster.on('exit', function (worker, code, signal) {
+        logger.error('worker ' + worker.process.pid + ' died');
+    });
+}else{
+    var server = app.listen(3001, function () {
+        logger.info('Express server listening on port 3001');
+    });
+}
+
